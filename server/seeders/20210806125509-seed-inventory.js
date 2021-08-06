1519a3e1935e082c93bdcb5d02819224653dc7f8
nugrahakiat <nugrahakiatsaputra@gmail.com>
2021-08-06 20:32:28 +0700

    Commit terakhir

--- server/seeders/20210806125509-seed-inventory.js
+++ server/seeders/20210806125509-seed-inventory.js
@@ -1,7 +1,17 @@
 'use strict';
-
+const fs = require('fs')
+let data = JSON.parse(fs.readFileSync('./databases/db.json','utf-8n'))
+data = data.map(el=>{
+  return {
+    ...el,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
+})
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert("Inventory",data,{})
+    await queryInterface.sequelize.query(`SELECT setval('"Inventory_id_seq"',(SELECT MAX(id) from "Inventory"))`)
     /**
      * Add seed commands here.
      *
@@ -14,6 +24,8 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Inventory")
+    await queryInterface.sequelize.query(`ALTER`)
     /**
      * Add commands to revert seed here.
      *
